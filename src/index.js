c93cef74be00490e2403109ee37c3fe32201d56c
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-11-05 20:47:54 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import * as Lodash from 'lodash.debounce';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const search = document.querySelector('#search-box')
+const list = document.querySelector('#country-list')
+const info = document.querySelector('#country-info')
+
+search.addEventListener('input', Lodash(searchQuery, DEBOUNCE_DELAY));
+
+function searchQuery() {
+	console.log(search.value);
+}
